f6e84a2c130718f9a0e8d8c61db4e7ddf84e665b
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2020-05-26 16:29:27 -0700

    add tests to script.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,15 +9,27 @@ using Distributed, Test, SlurmClusterManager
 jobid = read(`sbatch --export=ALL --parsable -n 4 -o test.out script.jl`, String)
 println("jobid = $jobid")
 
+# get job state from jobid
+getjobstate = jobid -> read(`sacct -j $jobid --format=state --noheader`, String)
+
 # wait for job to complete
 status = timedwait(60.0, pollint=1.0) do
-  state = read(`sacct -j $jobid --format=state --noheader`, String)
+  state = getjobstate(jobid)
   state == "" && return false
   state = first(split(state)) # don't care about jobsteps
   println("jobstate = $state")
   return state == "COMPLETED" || state == "FAILED"
 end
+
+# check that job finished running within timelimit (either completed or failed)
 @test status == :ok
 
-output = readlines("test.out")
-println(output)
+state = getjobstate(jobid) |> split
+# length should be two because creating the workers creates a job step
+@test length(state) == 2
+
+# check that everything exited without errors
+@test all(state .== "COMPLETED")
+
+# print job output
+read("test.out", String) |> print
--- test/script.jl
+++ test/script.jl
@@ -1,5 +1,13 @@
 #!/usr/bin/env julia
 
-using Distributed, SlurmClusterManager
+using Test, Distributed, SlurmClusterManager
 addprocs(SlurmManager())
-@everywhere println("hello from $(myid()):$(gethostname())")
+
+@test nworkers() == parse(Int, ENV["SLURM_NTASKS"])
+
+hosts = map(workers()) do id
+  remotecall_fetch(() -> gethostname(), id)
+end
+sort!(hosts)
+
+@test hosts == ["c1", "c1", "c2", "c2"]
