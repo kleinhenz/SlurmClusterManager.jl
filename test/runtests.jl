611efc86d9df2f0216bb41567fbcebb9d73b400b
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2021-04-09 14:35:26 -0700

    [ci] update ci
      * test on julia 1.6
      * set project correctly for submitted test script
      * improve output

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,12 @@ using Distributed, Test, SlurmClusterManager
 @test !(Sys.which("sinfo") === nothing)
 
 # submit job
-jobid = read(`sbatch --export=ALL --parsable -n 4 -o test.out script.jl`, String)
+# project should point to top level dir so that SlurmClusterManager is available to script.jl
+project_path = abspath(joinpath(@__DIR__, ".."))
+println("project_path = $project_path")
+jobid = withenv("JULIA_PROJECT"=>project_path) do
+  read(`sbatch --export=ALL --parsable -n 4 -o test.out script.jl`, String)
+end
 println("jobid = $jobid")
 
 # get job state from jobid
@@ -24,12 +29,14 @@ end
 # check that job finished running within timelimit (either completed or failed)
 @test status == :ok
 
+# print job output
+output = read("test.out", String)
+println("script output:")
+println(output)
+
 state = getjobstate(jobid) |> split
 # length should be two because creating the workers creates a job step
 @test length(state) == 2
 
 # check that everything exited without errors
 @test all(state .== "COMPLETED")
-
-# print job output
-read("test.out", String) |> print
--- test/script.jl
+++ test/script.jl
@@ -1,13 +1,13 @@
 #!/usr/bin/env julia
 
-using Test, Distributed, SlurmClusterManager
+using Distributed, SlurmClusterManager
 addprocs(SlurmManager())
 
-@test nworkers() == parse(Int, ENV["SLURM_NTASKS"])
+@assert nworkers() == parse(Int, ENV["SLURM_NTASKS"])
 
 hosts = map(workers()) do id
   remotecall_fetch(() -> gethostname(), id)
 end
 sort!(hosts)
 
-@test hosts == ["c1", "c1", "c2", "c2"]
+@assert hosts == ["c1", "c1", "c2", "c2"]
