365e4978b71fabc8ded7fa1f03b328df67ea08f6
Dilum Aluthge <dilum@aluthge.com>
2025-01-16 18:20:26 -0500

    Test suite: append to `JULIA_DEBUG`

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,13 @@ import Test
 # Bring some names into scope, just for convenience:
 using Test: @testset, @test
 
+const original_JULIA_DEBUG = strip(get(ENV, "JULIA_DEBUG", ""))
+if isempty(original_JULIA_DEBUG)
+  ENV["JULIA_DEBUG"] = "SlurmClusterManager"
+else
+  ENV["JULIA_DEBUG"] = original_JULIA_DEBUG * ",SlurmClusterManager"
+end
+
 # test that slurm is available
 @test !(Sys.which("sinfo") === nothing)
 
