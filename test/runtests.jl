9b847784410e7d9582d9a2551ada964da8af846d
Dilum Aluthge <dilum@aluthge.com>
2025-01-20 18:28:48 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ import Test
 # Bring some names into scope, just for convenience:
 using Test: @testset, @test
 
-function generate_JULIA_PROJECT_and_JULIA_LOAD_PATH()
+function generate_julia_env_vars()
   # Make sure to propagate `JULIA_PROJECT=SlurmClusterManager/Project.toml`.
   # This ensures that SlurmClusterManager.jl and Distributed.jl are available.
   #
@@ -24,7 +24,9 @@ function generate_JULIA_PROJECT_and_JULIA_LOAD_PATH()
   JULIA_PROJECT = SlurmClusterManagerROOTDIR_projecttoml                                        # SlurmClusterManager.jl/Project.toml
   JULIA_LOAD_PATH = "@$(directory_separator)$(SlurmClusterManagerROOTDIR_testdir)"              # @:SlurmClusterManager.jl/test
 
-  return (; JULIA_PROJECT, JULIA_LOAD_PATH)
+  env_vars = (; JULIA_PROJECT, JULIA_LOAD_PATH)
+
+  return env_vars
 end
 
 const original_JULIA_DEBUG = strip(get(ENV, "JULIA_DEBUG", ""))
@@ -37,12 +39,10 @@ end
 @testset "SlurmClusterManager.jl" begin
   # test that slurm is available
   @test !(Sys.which("sinfo") === nothing)
-  
-  # submit job
-  # project should point to top level dir so that SlurmClusterManager is available to script.jl
-  #
-  # 
-  (; JULIA_PROJECT, JULIA_LOAD_PATH) = generate_JULIA_PROJECT_and_JULIA_LOAD_PATH()
+
+  env_vars = generate_julia_env_vars
+  JULIA_PROJECT) = env_vars.JULIA_PROJECT
+  JULIA_LOAD_PATH = env_vars.JULIA_LOAD_PATH
   @info "" JULIA_PROJECT JULIA_LOAD_PATH
   
   # Use `sbatch` to submit the Slurm job.
